a1ea4c8df62f96e62a6e8346da77bbcf6e32cb46
Mohamad <139718328+mohamadmokarram@users.noreply.github.com>
2025-08-15 23:49:21 +0330

    Delete components directory

--- components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header({ sectionName, para }) {
-  return (
-    <div className="flex justify-between px-4 pt-4">
-      <h1>{sectionName}</h1>
-      <h1>{para}</h1>
-    </div>
-  );
-}
--- components/Main.js
+++
@@ -1,3 +0,0 @@
-export default function Main({ children }) {
-  return <main className="bg-gray-100 min-h-screen">{children}</main>;
-}
